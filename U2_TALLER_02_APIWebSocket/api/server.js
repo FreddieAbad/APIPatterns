e9c55bc070fcbc58f777477ccfd9072c911f2688
freddy <freddy.abadl@ucuenca.edu.ec>
2023-10-15 18:36:04 -0500

    Taller 3

--- U2_TALLER_02_APIWebSocket/api/server.js
+++ U2_TALLER_02_APIWebSocket/api/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const config = require('./config')
 const routes = require('./network/routes')
 const db = require('./db')
+// const winston = require('winston');
 
 var app = express()
 db( config.DB_URL )
@@ -14,6 +15,16 @@ app.use( body_parser.urlencoded({extended: false}) )
 app.use(express.static(path.join(__dirname, 'public')));
 
 routes( app )
-
+// // Configura un logger personalizado
+// const logger = winston.createLogger({
+//     level: 'info', // Nivel de registro
+//     format: winston.format.simple(), // Formato del registro
+//     transports: [
+//       new winston.transports.Console() // Salida de logs en la consola
+//     ]
+//   });
+  
 app.listen( config.PORT )
 console.log(`La aplicacion se encuentra arriba en http://localhost:${config.PORT}/`)
+// logger.info('Este es un mensaje de información.');
+// logger.error('Este es un mensaje de error.');
